b186b5b86acc43944f742bf11293f8992e5f2822
Mario09122004 <169079274+Mario09122004@users.noreply.github.com>
2025-03-12 18:40:53 -0600

    Delete email_test directory

--- email_test/src/routes/emailRout.js
+++
@@ -1,7 +0,0 @@
-import exepress from 'express';
-import { sendEmail } from '../controller/emailController.js';
-
-const router = exepress.Router();
-router.post('/send',sendEmail);
-
-export default router;
