8fd0ee7a1fd94f9d2c1ed6dcb68059028ed42f33
DiegoDaher <diego_3141201023@utd.edu.mx>
2025-03-30 09:39:02 -0600

    modelo producto esb-service TO ADD: controller

---
+++ Backend/ebs-service/src/main/java/com/utd/ti/soa/ebs_service/model/Product.java
@@ -0,0 +1,17 @@
+package com.utd.ti.soa.ebs_service.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private String name;
+    private String price;
+    private String dimensions;
+    private String weight;
+    private String description;
+    private String productMarlk;
+    private String material;
+    private String photo;
+}
